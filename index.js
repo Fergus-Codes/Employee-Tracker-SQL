3372d4825a42cb3dab4d2f1bdc46c55627d1f7cb
Fergus Illman <fergus.illman.au@gmail.com>
2023-06-02 19:59:38 +0930

    Created all functions - app working as intended. will work on improvements

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-
 // Import and require mysql2 & inquirer
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
 
 
-// Connect to database
+// Connect to SQL database
 const db = mysql.createConnection(
   {
     host: 'localhost',
     // MySQL username,
     user: 'root',
     // MySQL password
-    password: 'Dinofreads8!',
+    password: '',
     database: 'employee_db'
   },
   console.log(`Connected to the emplyoee_db database.`)
@@ -65,7 +64,9 @@ inquirer
 function viewAllDepartments () {
  //select * from departments
  db.query('SELECT * FROM department \G', function (err, results) {
+    console.log('|--------------------Result----------------------|')
     console.log(results)
+    console.log('|------------------------------------------------|')
     initUserPrompts()
 });
 };
@@ -73,7 +74,10 @@ function viewAllDepartments () {
 function viewAllRoles () {
 
     db.query('SELECT * FROM role \G', function (err, results) {
+        console.log('|--------------------Result----------------------|')
         console.log(results)
+        console.log('|------------------------------------------------|')
+
         initUserPrompts()
     });
  // select * from roles
@@ -82,7 +86,10 @@ function viewAllRoles () {
 function viewAllEmployees () {
 // select * from employees
 db.query('SELECT * FROM employee \G', function (err, results) {
+    console.log('|--------------------Result----------------------|')
     console.log(results)
+    console.log('|------------------------------------------------|')
+
     initUserPrompts()
 });
 };
@@ -98,7 +105,10 @@ function addADepartment () {
       ])
       .then((answers) => {
         if (answers.departmentname.length === 0 ) {
+        console.log('|--------------------Result----------------------|')
           console.log("Please enter a valid department name");
+        console.log('|------------------------------------------------|')
+
           initUserPrompts();
         } else {
           let newDepartment = answers.departmentname;
@@ -106,17 +116,20 @@ function addADepartment () {
           db.query('INSERT INTO department (name) VALUES (?);', [newDepartment], function (err, results) {
             if (err) {
               // error handling
+              console.log('|--------------------Result----------------------|')
               console.error("An error occurred while adding the department: ", err);
+              console.log('|------------------------------------------------|')
               return;
             }
+            console.log('|--------------------Result----------------------|')
             console.log(results);
             console.log(`The ${answers.departmentname} department has been added to the database`);
+            console.log('|------------------------------------------------|')
             initUserPrompts();
           });
         }
       });
-  };
-
+};
 
 function addARole () {
 
@@ -134,31 +147,44 @@ function addARole () {
       },
       {
         type: "input",
-        message: 'Please enter the role department',
+        message: 'Please enter the role department ID',
         name: "roledepartment"
       },
   ])
 
   .then((answers) => {
     if(answers.roletitle.length == 0) {
-
+        console.log('|--------------------Result----------------------|')
         console.log("Please enter a valid role title")
-
+        console.log('|------------------------------------------------|')
     } else {
     if(answers.rolesalary.length == 0) {
-
+        console.log('|--------------------Result----------------------|')
         console.log("Please enter a valid role salary")
-
+        console.log('|------------------------------------------------|')
     } else {
+
     if(answers.roledepartment.length == 0) {
-
+        console.log('|--------------------Result----------------------|')
         console.log("Please enter a valid role department")
-
+        console.log('|------------------------------------------------|')
     } else {
 
-// add all user input to create a new role
-console.log(`The ${answers.roletitle} role has been added to the database`)
-initUserPrompts()
+          // using placeholder '?' to avoid SQL injection attacks
+          db.query('INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?);', [answers.roletitle, answers.rolesalary, answers.roledepartment], function (err, results) {
+            if (err) {
+              // error handling
+              console.log('|--------------------Result----------------------|')
+              console.error("An error occurred while adding the role: ", err);
+              console.log('|------------------------------------------------|')
+              return;
+            }
+            console.log('|--------------------Result----------------------|')
+            console.log(results);
+            console.log(`The ${answers.roletitle} role has been added to the database`);
+            console.log('|------------------------------------------------|')
+            initUserPrompts();
+          });
     }}}
   })
 };
@@ -179,53 +205,110 @@ function addAnEmployee () {
       },
       {
         type: "input",
-        message: 'Please enter the role of the employee',
+        message: 'Please enter the role ID of the employee (must be a number)',
         name: "employeerole"
+      },
+      {
+        type: "input",
+        message: 'Please enter the employees manager ID (must be a number)',
+        name: "employeemanagerid"
       },
   ])
 
   .then((answers) => {
     if(answers.employeefirstname.length == 0) {
-
+        console.log('|--------------------Result----------------------|')
         console.log("Please enter a valid employee first name")
-
+        console.log('|------------------------------------------------|')
         return;
 
     } else {
     if(answers.employeelastname.length == 0) {
-
+        console.log('|--------------------Result----------------------|')
         console.log("Please enter a valid employee last name")
-
+        console.log('|------------------------------------------------|')
         return;
 
     } else {
     if(answers.employeerole.length == 0) {
-
-        console.log("Please enter a valid employee role")
-
+        console.log('|--------------------Result----------------------|')
+        console.log("Please enter a valid employee role ID")
+        console.log('|------------------------------------------------|')
         return;
 
     } else {
 // add all user input to create a new role
-console.log(`The ${answers.roletitle} role has been added to the database`)
+          // using placeholder '?' to avoid SQL injection attacks
+          db.query('INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?);', [answers.employeefirstname , answers.employeelastname , answers.employeerole, answers.employeemanagerid], function (err, results) {
+            if (err) {
+              // error handling\
+              console.log('|--------------------Result----------------------|')
+              console.error("An error occurred while adding the employee: ", err);
+              console.log('|------------------------------------------------|')
+              return;
+            }
+            console.log('|--------------------Result----------------------|')
+            console.log(results);
+            console.log(`${answers.employeefirstname} ${answers.employeelastname}  has been added to the employee database`);
+            console.log('|------------------------------------------------|')
+            initUserPrompts();
+          });
+
 initUserPrompts()
+
     }}}
   })
 };
 
-function updateEmployeeRole () {
-
+function updateEmployeeRole() {
     inquirer
-    .prompt([
-      {
-        type: 'list',
-        message: 'Please select from the following options below;',
-        choices: ["View all departments", "View all roles", "View all employees", "Add a department", "Add a role", "Add an employee", "Update an employee role"],
-        name:"userchoice"
-      }
-  ])
+      .prompt([
+        {
+          type: 'input',
+          message: 'Please enter the employee ID of the employee you would like to update.',
+          name: 'updateemployeeid'
+        },
+        {
+          type: 'list',
+          message: 'Please select what you would like to update on the employee\'s record.',
+          choices: ["first_name", "last_name", "role_id", "manager_id"],
+          name: 'updateemployeefield'
+        },
+        {
+          type: 'input',
+          message: 'Please enter the value that you would like to replace the current value with on the employee\'s record.',
+          name: 'updatecontent'
+        },
+      ])
+      .then((answers) => {
+        if (answers.updateemployeeid.length === 0) {
+            console.log('|--------------------Result----------------------|')
+          console.log('Please ensure you enter a valid employee ID');
+          console.log('|------------------------------------------------|')
 
-console.log(`The `)
+        } else {
+          let updateSelectedEmployee = answers.updateemployeeid;
+          let updateField = '`' + answers.updateemployeefield + '`'; // Enclose field name within backticks
+          let updateContent = answers.updatecontent;
+  
+          let sql = 'UPDATE employee SET ' + updateField + ' = ? WHERE id = ?';
+          let values = [updateContent, updateSelectedEmployee];
+  
+          db.query(sql, values, function (err, results) {
+            if (err) {
+                console.log('|--------------------Result----------------------|')
+              console.error('An error occurred while updating the employee:', err);
+              console.log('|------------------------------------------------|')
+              return;
+            }
+            console.log('|--------------------Result----------------------|')
+            console.log(results);
+            console.log(`The records for employee ${updateSelectedEmployee} have been updated in the database`);
+            console.log('|------------------------------------------------|')
+            initUserPrompts();
+          });
+        }
+      });
 };
 
 initUserPrompts();
